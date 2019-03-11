c3a51ba929d7d4364a08c9bb5fe33dd7ffde6b87
beattykariuki <bettykariuki026@gmail.com>
2019-03-12 00:39:50 +0300

    created a url for the search function

--- gram/urls.py
+++ gram/urls.py
@@ -1,7 +1,17 @@
 from django.conf.urls import url
 from . import views
+from .django.conf import settings
+from django.conf.urls.static import static
+from django.shortcuts import render, redirect
 
 #Create your Urls here
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome')
+    url('^$',views.welcome,name = 'welcome'),
+    url(r'^search/',views.search_results,name='search_results')
+    url(r'^explore/$',views.explore,name='explore'),
+    url(r'^notification/$',views.notification,name='notification'),
+    url(r'^profile/$',views.profile,name='profile'),
+    url(r'^upload/$',views.upload,name='upload'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
